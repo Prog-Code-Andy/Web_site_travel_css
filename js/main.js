ebb47086d0f122c3026f9c74780876642a84d3ed
space_irishman <himzpua@gmail.com>
2021-12-16 08:30:33 +0200

    16.12.21

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
     $(".mobile-menu").click(function(){
-        $(".mobile-menu").toggleClass("active");
+        $(".mobile-menu").toggleClass("active"); /* toggleClass > bool */
     })
+    $(".carusel-slider").slick({
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        arrows: false
+    });
 });
